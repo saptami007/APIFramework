89b675f4c850bcf2a1f6178df9f2f0a32079651e
subhadeepmondal <subhadeepmondal@subhadeeps-mbp.lan>
2019-08-11 19:28:55 -0400

    Updated the GET API with retrieving different values using Maps and Lists

--- APIFramework/src/test/java/settingAPI/TC001_GetAPI_SetUp.java
+++ APIFramework/src/test/java/settingAPI/TC001_GetAPI_SetUp.java
@@ -10,6 +10,10 @@ import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import io.restassured.response.Response;
 
@@ -20,6 +24,8 @@ public class TC001_GetAPI_SetUp {
 		
 		String url=ReadConfigFile.getConfigData("url");
 		System.out.println("The URL is : "+url);
+		String contentType=ReadConfigFile.getConfigData("contentType");
+		System.out.println("The Content Type is "+contentType);
 		response = given().
 				when().
 				get(url).
@@ -27,6 +33,14 @@ public class TC001_GetAPI_SetUp {
 				extract().
 				response();
 		System.out.println("The Json is: " + response.asString());
+		List<Map<String, Object>> circuits=new ArrayList<Map<String,Object>>();
+		circuits=response.jsonPath().get("MRData.CircuitTable.Circuits");
+		for(Map<String,Object>index : circuits) {
+			System.out.println("The Circuit Ids are: "+index.get("circuitId"));
+			Map<String,Object> location=new HashMap<String, Object>();
+			location=(Map<String, Object>) index.get("Location");
+				System.out.println("The Latitudes are: "+location.get("lat"));
+		}
 		  
 	}
 
